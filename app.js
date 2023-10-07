f35493af12b6196d94973d62d43dfbe2abf10972
Pratham2301 <prathamrajbhoj2003@gmail.com>
2023-10-08 02:37:37 +0530

    cors ✅

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://rcoem-coderz.netlify.app/', '*'],
+    origin: [
+        'http://localhost:5173', 
+        'https://rcoem-coderz.netlify.app', 
+        'https://rcoem-coderz.netlify.app/', 
+        'https://rcoem-coderz.netlify.app/*', 
+        '*'
+    ],
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
     credentials: true,
     exposedHeaders: ['set-cookie'],
