3fd106bc06fc392774f5f2a4871b1177ea8df478
Pratham2301 <prathamrajbhoj2003@gmail.com>
2023-11-16 16:50:28 +0530

    gfg ranklist schema ✅

--- models/ranklistModel.js
+++ models/ranklistModel.js
@@ -75,6 +75,24 @@ const ranklistSchema = new mongoose.Schema(
                 geeksforgeeks_rating: { type: Number }
             },
         ],
+        geeksforgeeks_ranklist: [
+            {
+                name: { type: String },
+                email: { type: String },
+                total_score: { type: Number },
+                codechef_id: { type: String },
+                codechef_rating: { type: Number },
+                codeforces_id: { type: String },
+                codeforces_rating: { type: Number },
+                leetcode_id: { type: String },
+                leetcode_rating: { type: Number },
+                github_id: { type: String },
+                avatar: { type: String },
+                college_name: { type: String },
+                geeksforgeeks_id: { type: String },
+                geeksforgeeks_rating: { type: Number }
+            },
+        ],
         updatedAt: {
             type: Date,
             default: Date.now,
